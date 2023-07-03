8d5283ee1fb74829554647aca340b67793452cce
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-03 21:31:49 +0330

    Create 4.py
    sign in

---
+++ assign4/4.py
@@ -0,0 +1,8 @@
+username1 ='nooria'
+password1 = '5555'
+username = input("enter username: ")
+password = input("enter password: ")
+
+if username1 == username and password1 == password :
+    print('log in')
+else: print('Enter your username and password again')
