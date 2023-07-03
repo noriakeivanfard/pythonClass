743f11100d4d5bc09eb559d67a8abf3591d215a6
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-03 22:42:11 +0330

    Create 6.py

---
+++ assign4/6.py
@@ -0,0 +1,10 @@
+number = float(input("enter number: "))
+if number < 0 :
+    print('D')
+else :
+    if (number // 2) * 2 == number > 6 :
+     print('A')
+    elif number < 4 :
+     print('B')
+    else:
+     print('c')
