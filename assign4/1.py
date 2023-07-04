6e46c1841e6e2e5e6c63f574cdcf685a26a4f907
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-04 11:18:28 +0330

    Create 1.py

---
+++ assign4/1.py
@@ -0,0 +1,6 @@
+import math 
+number = float(input("enter youre number: "))
+ceil   = math.ceil(number)
+floor  = math.floor(number)
+print("math.ceil({0}) = {1}".format(number, ceil))
+print("math.floor({0})   = {1}".format(number, floor))
