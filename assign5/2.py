957291cc652821a8799e2466095eb200e7c91eae
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-05 12:45:56 +0330

    Update 2.py

--- assign5/2.py
+++ assign5/2.py
@@ -1,10 +1,20 @@
 #title, istitle
-txt = "hello my butiful word"
+txt = "hello my beautiful word"
 x = txt.title()
 print(x)
 
 ##############################
 
-txt = "hello my butirul word"
+txt = "hello my beautiful word"
 x = txt.istitle()
 print(x)
+
+###############################
+#find
+text = "hello my beautiful word"
+x = text.find("beautiful")
+print(x)
+
+##############################
+#replace
+
