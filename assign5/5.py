1e92765e770a942ab6c2e13ee0d1722c7e63b28f
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-06 10:19:56 +0330

    Update 5.py

--- assign5/5.py
+++ assign5/5.py
@@ -4,6 +4,6 @@ if number % 7 == 0:
     print(f"{number} is multiple 7")
 else:
     print(f"{number} is not multiple 7 ")
-    number += 1 
     number *= 7
+    number += 7
     print(f"{number}Close to a multiple 7")
