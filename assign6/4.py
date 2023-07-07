127d32a640564bdbfb93fceb157f307ba12dd004
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-07 20:42:25 +0330

    Create 4.py

---
+++ assign6/4.py
@@ -0,0 +1,16 @@
+list_name = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
+
+name1 = input("enter name: ")
+number1 = int(input("enter number: "))
+
+name2 = input("enter name: ")
+number2 = int(input("enter number: "))
+
+name3 = input("enter name: ")
+number3 = int(input("enter number: "))
+
+list_name.insert(number1,name1)
+list_name.insert(number2,name2)
+list_name.insert(number3,name3)
+
+print(list_name)
