ab317c8686812791b38356ab8b2cb19c3ae58a4e
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-08 20:02:13 +0330

    Update 𝑩𝒂𝒌𝒆𝒓𝒚.py

--- assign6/𝑩𝒂𝒌𝒆𝒓𝒚.py
+++ assign6/𝑩𝒂𝒌𝒆𝒓𝒚.py
@@ -2,15 +2,15 @@ list_name = ["ali", "atefe", "reza", "homa", "amir", "fateme"]
 
 name1 = input("enter name: ")
 number1 = int(input("enter number: "))
+list_name.insert(number1 - 1 , name1)
+print(list_name)
 
 name2 = input("enter name: ")
 number2 = int(input("enter number: "))
+list_name.insert(number2 - 1 , name2)
+print(list_name)
 
 name3 = input("enter name: ")
 number3 = int(input("enter number: "))
-
-list_name.insert(number1,name1)
-list_name.insert(number2,name2)
-list_name.insert(number3,name3)
-
+list_name.insert(number3 - 1 ,name3)
 print(list_name)
