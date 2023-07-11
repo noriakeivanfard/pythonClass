cf1f0592f74211bb100fd85928978f649a27411b
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-11 07:08:52 +0330

    Create ATM.py

---
+++ assign7/ATM.py
@@ -0,0 +1,18 @@
+while True:
+ password_l = '1389'
+ list_password = []
+ c = 0
+ password = int(input('enter password:'))
+
+ if password_l == 4:
+            if password_l == password:
+             print('log in')
+            
+
+            elif (password_l) == (list_password[::-1]):
+             print('calling to police')
+            
+            else:
+             print('please try again')
+ else:
+    print('lock')
