e28220be3b86957bc497d89cd5dddddb9727407d
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-12 20:04:00 +0330

    Update 𝒈𝒖𝒆𝒔𝒔𝒊𝒏𝒈 𝒏𝒖𝒎𝒃𝒆𝒓.py

--- assign8/𝒈𝒖𝒆𝒔𝒔𝒊𝒏𝒈 𝒏𝒖𝒎𝒃𝒆𝒓.py
+++ assign8/𝒈𝒖𝒆𝒔𝒔𝒊𝒏𝒈 𝒏𝒖𝒎𝒃𝒆𝒓.py
@@ -1,21 +1,18 @@
 import random
 number = int(input('enter number:'))
-c = 0
 a = 0
 b = 100
-input_number = random.randint(a,b)
 while True:
+    input_number = random.randint(a,b)
     if input_number == number :
-             print(f" your number:{input_number}", "is win!")
-             c += 1
-             break
+        print(f" your number:{input_number}", "is win!")
+        break
     elif input_number > number:
-            print(f" your number:{input_number}", "payintar")
-            num = input_number
-            input_number = random.randint(a,b)
+        print(f" your number:{input_number}", "payintar")
+        b = input_number
+        input_number = random.randint(a,b)
     else: 
         input_number < number
         print(f" your number:{input_number}", "balatar")
-        num = input_number
+        a = input_number
         input_number = random.randint(a,b)
-
