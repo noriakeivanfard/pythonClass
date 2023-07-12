3b2113a262756463ca4f51f5f068be7c98b5ce73
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-12 13:01:07 +0330

    Create 𝑫𝒊𝒄𝒆.py

---
+++ assign8/𝑫𝒊𝒄𝒆.py
@@ -0,0 +1,13 @@
+import random
+numbers = 0
+list_number = []
+c = 0
+while (numbers != 6): 
+    list_number.append(numbers)
+    numbers = random.randint(1, 6)
+    c += 1
+    if numbers == 6:
+        print(f"round {c} is win")
+    else:
+        print(f"round {c} is:", numbers)
+
