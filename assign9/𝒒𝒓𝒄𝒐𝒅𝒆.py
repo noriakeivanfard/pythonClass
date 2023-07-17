f2c0ad11b95549c714285e43ad11516b7818488c
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-17 09:44:02 +0330

    Update 𝒒𝒓𝒄𝒐𝒅𝒆.py

--- assign9/𝒒𝒓𝒄𝒐𝒅𝒆.py
+++ assign9/𝒒𝒓𝒄𝒐𝒅𝒆.py
@@ -1,4 +1,4 @@
 import qrcode
-qr = qrcode.make("i'm nooria \n my email:noriak2009@gmail.com \n bye bye'🙋‍♀️")
+qr = qrcode.make("i'm nooria \n my email:noriak2009@gmail.com \n bye bye🙋‍♀️")
 
 qr.save('qrcode.png',scale=10 , dark_dark='darkgreen ', light='lightgreen', )
