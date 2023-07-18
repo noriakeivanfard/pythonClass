8237b30acad27a3417dc4270b405b71e76592625
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-18 12:00:43 +0330

    Update 𝒅_𝒎_𝒉.py

--- assign10/𝒅_𝒎_𝒉.py
+++ assign10/𝒅_𝒎_𝒉.py
@@ -1,7 +1,16 @@
-number = int(input("enter age: "))#13
+import datetime
+today = datetime.datetime.today()
+today_year = (today.year)
+day = int(input("enter day : "))
+month = int(input("enter month: "))
+year = int(input("enter year : "))
 
-day = number*365
-month = number*12
-hour = day*24
+age = today_year - year
 
-print(f"day:{day},month:{month},hour:{hour}")
+print(f"You {age} years old ")
+
+day = age * 365
+week = age * 52       
+second = age * 31536000
+
+print(f" weeks:{week} \n days:{day} \n seconds:{second} ")
