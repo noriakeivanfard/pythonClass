3a98136ea08619d4a7ab6c22237ff07d6fee6f36
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-20 01:00:34 +0330

    Update 𝒊𝒔𝒑𝒓𝒊𝒎𝒆.py

--- assign11/𝒊𝒔𝒑𝒓𝒊𝒎𝒆.py
+++ assign11/𝒊𝒔𝒑𝒓𝒊𝒎𝒆.py
@@ -1,20 +1,12 @@
-number = int(input('enter your number: '))
-active = True
-while active:
-    if number <= 1: 
+def isprime(number):
+    n = 0
+    for i in range(1, number+1):
+        if number%i == 0:
+            n+=1
+    if n==2:
+        print('your number is prime')
+    else:
         print('your number is not prime')
-        break
-    if number <= 3: 
-        print('your number is prime')  
-        break
-    if number % 2 == 0 or number % 3 == 0: 
-        print('your number is not prime')
-        break
-    i = 5
-    while i * i <= number: 
-        if number % i == 0 or number % (i + 2) == 0: 
-            print('your number is not prime')
-            break
-        i = i + 6
-    active = False
-    print('your number is prime') 
+        
+number = int(input("please enter your number :"))
+print(isprime(number))
