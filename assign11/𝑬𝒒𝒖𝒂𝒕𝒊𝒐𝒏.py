ffe643382f7ac7492bc4191b8b1c50d432e420fe
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-20 06:59:05 +0330

    Create 𝑬𝒒𝒖𝒂𝒕𝒊𝒐𝒏.py

---
+++ assign11/𝑬𝒒𝒖𝒂𝒕𝒊𝒐𝒏.py
@@ -0,0 +1,14 @@
+def Equation(a,b,c,d):
+
+ a = float(input("please enter number1: "))
+
+ b = float(input("please enter number2: "))
+
+ c =float(input("please enter number3: "))
+ 
+ d = float(input("please enter number4: "))
+
+ return (a*d)**2 + (b*d) + c
+
+print(Equation(1,2,3,4))
+
