9d8ac97df582808e76d62119f45e22d10ba193c1
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-21 23:15:36 +0330

    Update 𝑬𝒒𝒖𝒂𝒕𝒊𝒐𝒏.py

--- assign11/𝑬𝒒𝒖𝒂𝒕𝒊𝒐𝒏.py
+++ assign11/𝑬𝒒𝒖𝒂𝒕𝒊𝒐𝒏.py
@@ -1,14 +1,15 @@
-def Equation(a,b,c,d):
+import math
+def Equation(a , b , c ):
+    a = float(input("enter number 1:"))
+    b = float(input("enter number 2:"))
+    c = float(input("enter number 3:"))
+    num = (b ** 2-4*a*c)
+    if num > 0:
+        x1 = (-b + (math.sqrt(num))) / (2 * a)
+        x2 = (-b - (math.sqrt(num))) / (2 * a)
+        print(x1 , x2)
+    elif num == 0 :
+        x3 = (-b / (2 * a))
+        print(x3)
 
- a = float(input("please enter number1: "))
-
- b = float(input("please enter number2: "))
-
- c =float(input("please enter number3: "))
- 
- d = float(input("please enter number4: "))
-
- return (a*d)**2 + (b*d) + c
-
-print(Equation(1,2,3,4))
-
+print(Equation(1,2,3 ))
