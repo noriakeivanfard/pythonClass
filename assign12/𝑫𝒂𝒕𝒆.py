f52515ef0363c81f3fa1f989137a5061360363c1
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-22 20:18:26 +0330

    Update 𝑫𝒂𝒕𝒆.py

--- assign12/𝑫𝒂𝒕𝒆.py
+++ assign12/𝑫𝒂𝒕𝒆.py
@@ -2,6 +2,6 @@ def date(day, month, year):
     day = int(input("day: "))
     month = int(input("month: "))
     year = int(input("year: "))
-    return f"{year}/{month}/{day}"
+    print(year, "/", month, "/", day, sep="")
 
 print(date(1,2,3))
