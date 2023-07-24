28817d9488ef107800aeae49a8162f4ea269ec06
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-24 13:53:31 +0330

    Create 𝒅𝒆𝒅𝒖𝒄𝒕𝒊𝒐𝒏.py

---
+++ assign13/𝒅𝒆𝒅𝒖𝒄𝒕𝒊𝒐𝒏.py
@@ -0,0 +1,29 @@
+s_1 = 5
+m_1 = 12
+
+s_2 = 25
+m_2 = 100
+
+sum_answer_s = (s_1 * m_2) + (s_2 * m_1)
+sum_answer_m = m_1 * m_2
+
+minus_answer_s = (s_1 * m_2) - (s_2 * m_1)
+minus_answer_m = m_1 * m_2
+
+multiplication_answer_s = s_1 * s_2
+multiplication_answer_s = m_1 * m_2
+
+#Division_answer_s = s_2, m_2[::-1]
+#Division_answer_m = s_1 and m_1 * Division_answer_s
+
+print( sum_answer_s,'\n ------ \n', sum_answer_m)
+
+print("________________")
+
+print(minus_answer_s,'\n ----- \n',minus_answer_m)
+
+print("________________")
+
+print(multiplication_answer_s , '\n ----- \n',multiplication_answer_s )
+#print(Division_answer_m)
+
