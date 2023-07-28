966e733a82eb572e546687f18f8a69bd6ea3d4dd
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-28 23:44:58 +0330

    Create 𝒐𝒑𝒑.py

---
+++ assign14/𝒐𝒑𝒑.py
@@ -0,0 +1,18 @@
+class Teacher:   
+    # Properties
+    name = None
+    last_name = None
+    age = None
+    Field_Study = None
+    
+    # Methods
+    def Asking_questions_students(): 
+        pass 
+    def exam(): 
+        pass
+    def Check_homework(): 
+        pass
+    def write_the_Students_name(): 
+        pass 
+
+##############################################################################3
