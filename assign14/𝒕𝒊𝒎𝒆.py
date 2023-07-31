779b33954244b4adc93ee40ecabba7e869a6b76a
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-07-31 18:27:14 +0330

    Update 𝒕𝒊𝒎𝒆.py

--- assign14/𝒕𝒊𝒎𝒆.py
+++ assign14/𝒕𝒊𝒎𝒆.py
@@ -1,49 +1,61 @@
-class time:
-    def __init__(self,h1,h2,m1,m2,s1,s2):
-    # Properties
-        self.hour_s = h1 + h2
-        self.minut_s = m1 + m2
-        self.second_s = s1 + s2
-        self.hour_m = h1 - h2
-        self.minut_m = m1 - m2
-        self.second_m = s1 - s2
-        self.op = op
-    # Methods
+def check(h, m, s):
+    while not(0 <= s <= 60 and 0 <= m <= 60):
+        if s >= 60:
+            s -= 60
+            m += 1
+        if s < 0:
+            m -= 1
+            s += 60
+        if m >= 60:
+            m -= 60
+            h += 1
+        if m < 0:
+            h -= 1
+            m += 60
+    return h,m,s
+
+class Time:
+    def __init__(self, h1, h2, m1, m2, s1, s2):
+        self.h1 = h1
+        self.h2 = h2
+        self.m1 = m1
+        self.m2 = m2
+        self.s1 = s1
+        self.s2 = s2
+
     def sum(self):
-        while not(0 <= self.second_s <= 60 and 0 <= self.minut_s <= 60):
-            if self.second_s >= 60:
-                self.second_s -= ((self.second_s // 60) * 60)
-                self.minut_s += (self.second_s // 60)
-            if self.minut_s >= 60:
-                self.minut_s -= ((self.minut_s // 60) * 60)
-                self.hour_s += (self.minut_s // 60)
- 
+        h, m, s = self.h1 + self.h2, self.m1 + self.m2, self.s1 + self.s2
+        h, m, s = check(h, m, s)
+        return h,m,s
+    
     def min(self):
-            while not(0 <= self.second_m <= 60 and 0 <= self.minut_m <= 60):
-                if self.second_m < 0:
-                    self.minut_m -= 1
-                    self.second_m += 60
-                    if self.minut_m < 0:
-                        self.hour_m -= 1
-                        self.minut_m += 60
+        h, m, s = self.h1 - self.h2, self.m1 - self.m2, self.s1 - self.s2
+        h, m, s = check(h, m, s)
+        return h,m,s
 
-def show_time(h,m,s):
-    print(f"{h}:{m}:{s}")
+    def show(self, h, m, s):
+        print(f"{h}:{m}:{s}")
 
-time1 = input("enter time1: ")
-time2 = input("enter time2: ")
-op=int(input("""
+time1 = input("enter time1:like(00:00:00) ")
+time2 = input("enter time2:like(00:00:00) ")
+op = int(input("""
 1_sum
 2_min
 which one?"""))
-time1 = time1.split(":")
-time2 = time2.split(":")
-for i in time1:
- time1 = [eval(i)]
-for i in time2:
- time2 = [eval(i)]
-t = time(time1[0], time1[1], time1[2], time2[0], time2[1], time2[2], op)
+            
+t1 = time1.split(":")
+t2 = time2.split(":")
+
+time = Time(int(t1[0]), 
+            int(t2[0]), 
+            int(t1[1]), 
+            int(t2[1]), 
+            int(t1[2]), 
+            int(t2[2])) 
+
 if op == 1:
-    t.sum()
+    h, m, s = time.sum()
+    time.show(h, m, s)
 elif op == 2:
-    t.min() 
+    h, m, s = time.min()
+    time.show(h, m, s) 
