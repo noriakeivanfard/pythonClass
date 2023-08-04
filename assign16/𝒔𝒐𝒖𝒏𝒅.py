ea869250205e954c6c577111cc3bb9a677b0d592
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-04 09:17:00 +0330

    Create 𝒔𝒐𝒖𝒏𝒅.py

---
+++ assign16/𝒔𝒐𝒖𝒏𝒅.py
@@ -0,0 +1,23 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("please type your str_1")
+engine.runAndWait()
+str_1 = input("enter your str_1: ")
+engine.say("please type your str_2")
+engine.runAndWait()
+str_2 = input("enter your str_2: ")
+engine.say("please type your str_3")
+engine.runAndWait()
+str_3 = input("enter your str_3: ")
+if len(str_1) > len(str_2) > len(str_3):
+    Result = str_1
+    engine.say(f" The longest string{str_1}")
+    engine.runAndWait()
+elif len(str_2) > len(str_1) > len(str_3):
+    Result = str_2
+    engine.say(f" The longest string{str_2}")
+    engine.runAndWait()
+elif len(str_3) > len(str_1) > len(str_2):
+    Result = str_3
+    engine.say(f" The longest string{str_3}")
+    engine.runAndWait() 
