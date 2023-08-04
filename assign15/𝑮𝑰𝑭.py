9667598e2805d0cc9492fd2404fbbe0a45f598a7
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-04 18:43:25 +0330

    Update 𝑮𝑰𝑭.py

--- assign15/𝑮𝑰𝑭.py
+++ assign15/𝑮𝑰𝑭.py
@@ -1,1 +1,10 @@
+import os
+import imageio
+
+images = []
+img = os.listdir("images")
+for name in sorted(img):
+    images.append(imageio.imread("images/" + name))
+
+imageio.mimsave("output.gif",images) 
 
