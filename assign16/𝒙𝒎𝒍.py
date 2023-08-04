187fdacca3773551f4b21d10a5dac657513fc34d
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-04 14:05:39 +0330

    Create 𝒙𝒎𝒍.py

---
+++ assign16/𝒙𝒎𝒍.py
@@ -0,0 +1,12 @@
+import xml.etree.ElementTree as ET
+tree = ET.parse('file.xml')
+root = tree.getroot()
+for child in root.iter('child'):
+    print(child.tag, child.attrib)
+    
+for element in root.iter('element'):
+    print(element.text)
+
+new_element = ET.SubElement(root, 'new_element')
+new_element.text = 'Hello World'
+tree.write('output.xml') 
