5258b2cdf214934507780eaae1c467b884473206
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-05 09:47:31 +0330

    Update 𝒇𝒊𝒍𝒆.py

--- assign15/𝒇𝒊𝒍𝒆.py
+++ assign15/𝒇𝒊𝒍𝒆.py
@@ -1,1 +1,14 @@
+import os
+import shutil
 
+cwd = os.getcwd()
+img = os.listdir()
+a = cwd+"/folder_images"
+os.mkdir(a) 
+list_images = ["jpeg", "heic", "png", "tiff", "gif", "jpeg", "raw"]
+
+for i in img:
+    if len(i.split(".")) > 1:
+        for j in list_images:
+            if i.split(".")[1] == j:
+                shutil.copy(i, a)
