0b3b2be1c686da252377e8d72a98bb8a97d5ad6a
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-06 13:32:48 +0330

    Update 𝒑_𝒕_𝒔.py

--- assign15/𝒑_𝒕_𝒔.py
+++ assign15/𝒑_𝒕_𝒔.py
@@ -1,17 +1,10 @@
 class Pyramid:
     # Properties
     radius = None
-    def __init__(self, s):
-        self.radius = s
-    
-    def set_radius(self, s):
-        self.radius = s
-        
-    def __init__(self, h):
-        self.radius = h
-        
-    def set_radius(self, h):
-        self.radius = h
+    def __init__(self, s1):
+        self.radius = s1
+    def __init__(self, s2):
+        self.radius = s2
         
     def Perimeter(self):
         return (self.radius * self.radius) // 1.3
@@ -20,16 +13,13 @@ class Square(Pyramid):
         def __init__(self, a):
             self.radius = a
             
-        def set_radius(self, a):
-            self.radius = a
-            
         def Area(self):
-            return self.radius * self.radius
+            return self.radius ** 2
         
         def Perimeter(self):
             return self.radius *4 
         
-class triangle(Square):
+class Triangle(Square):
             def __init__(self, a):
                 self.radius = a
                 
@@ -43,8 +33,12 @@ class triangle(Square):
                 self.radius = h
                 
             def Area(self):
-                return (self.radius * self.radius)//2
+                return (self.h * self.b)//2
             
             def Perimeter(self):
-                return self.radius + self.radius + self.radius
-    
+                return self.a + self.b + self.c
+            
+pyramid =  Pyramid(5)
+print(pyramid.Perimeter())
+sphere = Square(5)
+triangle = Triangle(5) 
