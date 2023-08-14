77b67d20131677182ad8736125b4d07a2b9f3f86
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-08-14 08:31:37 +0330

    Update 2𝑫 𝑺𝒑𝒂𝒄𝒆 𝑺𝒉𝒐𝒐𝒕𝒆𝒓.py

--- assign18/2𝑫 𝑺𝒑𝒂𝒄𝒆 𝑺𝒉𝒐𝒐𝒕𝒆𝒓.py
+++ assign18/2𝑫 𝑺𝒑𝒂𝒄𝒆 𝑺𝒉𝒐𝒐𝒕𝒆𝒓.py
@@ -1,1 +1,19 @@
+import arcade
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+
+class MyGame(arcade.Window):
+    def __init__(self, w, h):
+        super().__init__(w, h)
+        arcade.load_texture("img/background.png")
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.run()
+
+if __name__ == "__main__":
+    main() 
