ff51221ed1d6630a65847e8a9b6f56139686d076
nooria <137643989+noriakeivanfard@users.noreply.github.com>
2023-09-02 13:13:00 +0330

    Update 𝒄𝒂𝒍𝒄𝒖𝒍𝒂𝒕𝒐𝒓.py

--- assign19/𝒄𝒂𝒍𝒄𝒖𝒍𝒂𝒕𝒐𝒓.py
+++ assign19/𝒄𝒂𝒍𝒄𝒖𝒍𝒂𝒕𝒐𝒓.py
@@ -3,9 +3,6 @@ from functools import partial
 from PySide6.QtWidgets import QApplication
 from PySide6.QtUiTools import QUiLoader
 
-num_1 = 0
-num_2 = 0
-
 def stylesheet():
     main_window.Button_0.setStyleSheet(''' QPushButton {font: 700 italic 14pt "Segoe UI";background-color:rgb(0, 85, 0);color: rgb(255, 255, 255);}''')
     main_window.Button_1.setStyleSheet(''' QPushButton {font: 700 italic 14pt "Segoe UI";background-color:rgb(0, 85, 0);color: rgb(255, 255, 255);}''')
